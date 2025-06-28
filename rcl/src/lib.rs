e4132408606ab1e783a6fb5adc1c2a5766425611
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2025-06-27 19:18:58 -0700

    Updating the interface to handle connections, updating loader next (accepting breakage)

--- rcl/src/lib.rs
+++ rcl/src/lib.rs
@@ -2,14 +2,31 @@
 //!
 //! This is the crate defining how the coordinator should load and call modules built by rancid cache
 #![forbid(unsafe_code)]
-#![warn(missing_docs)]
+use async_trait::async_trait;
+use thiserror::Error;
+use tokio::io::{AsyncRead, AsyncWrite};
+
+pub trait AsyncStreamInterface: AsyncRead + AsyncWrite + Send {}
+impl<T> AsyncStreamInterface for T where T: AsyncRead + AsyncWrite + Send {}
 
 /// RCL Plugin Interface
 ///
-/// This trait is used to allow a coordinator to know how to load and control configured modules. It is not intended to be directly used.
+/// This trait is used to allow a coordinator to know how to load and control configured modules. It is not intended to be directly used outside this codebase.
+#[async_trait]
 pub trait RclTrait {
-    /// Starts up the module for processing, this signature will likely change a LOT
-    fn start(&self) -> Result<String, String>;
+    fn start(&self);
+
+    async fn handle_connection(
+        &self,
+        stream: Box<dyn AsyncStreamInterface>,
+    ) -> Result<(), RclPluginError>;
+}
+
+/// Errors that should be communicated back to the coordinator
+#[derive(Error, Debug)]
+pub enum RclPluginError {
+    #[error("Handling Error {0}")]
+    Handling(String),
 }
 
 /// This type is to allow for a construction function for the plugins, its TBD if the construction function needs to be mangled
@@ -17,30 +34,46 @@ pub type RclPlugin = Box<dyn RclTrait>;
 
 #[cfg(test)]
 mod tests {
+    use tokio_test::io::Builder;
+
     use super::*;
 
     struct RclTest;
+    #[async_trait]
     impl RclTrait for RclTest {
-        fn start(&self) -> Result<String, String> {
-            Result::Ok("Works".into())
+        fn start(&self) {}
+        async fn handle_connection(
+            &self,
+            _stream: Box<dyn AsyncStreamInterface>,
+        ) -> Result<(), RclPluginError> {
+            println!("Connection handled");
+            Ok(())
         }
     }
 
-    #[test]
-    fn dummy_impl() {
+    #[tokio::test]
+    async fn dummy_impl() -> Result<(), anyhow::Error> {
         let test = RclTest;
-        let output = test.start();
+        test.start();
 
-        assert_eq!(*"Works", *output.unwrap());
+        let mut builder = Builder::new();
+        let mock = Box::new(builder.build());
+
+        test.handle_connection(mock).await?;
+        Ok(())
     }
 
-    #[test]
-    fn trait_test() {
+    #[tokio::test]
+    async fn trait_test() -> Result<(), anyhow::Error> {
         let obj = Box::new(RclTest);
         let dynobj = <RclPlugin>::from(obj);
 
-        let output = dynobj.start();
+        dynobj.start();
 
-        assert_eq!(*"Works", *output.unwrap());
+        let mut builder = Builder::new();
+        let mock = Box::new(builder.build());
+
+        dynobj.handle_connection(mock).await?;
+        Ok(())
     }
 }
